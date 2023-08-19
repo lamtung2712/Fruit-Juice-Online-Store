8506b7d997d54b7e7af86174ed8fb3ae33309719
lamtung2712 <ntungnam2789@gmail.comm>
2023-08-19 20:13:38 +0700

    commit again

--- Core/urls.py
+++ Core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views import HomeView
+from .views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
--- Store/settings.py
+++ Store/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'Shipping',
     'ShippingLine',
     'Core',
-
+    'Store'
 ]
 
 MIDDLEWARE = [
