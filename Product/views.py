0b06c3b23c812041173525e40caea61c00495f77
lamtung2712 <ntungnam2789@gmail.comm>
2023-08-21 23:13:45 +0700

    product_section

--- Product/views.py
+++ Product/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def product_list(request):
+    products = Product.objects.filter(active=True)
+    return render(request, 'homepage/product_section.html', {'products': products})
--- Store/settings.py
+++ Store/settings.py
@@ -135,6 +135,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
