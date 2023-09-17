e8f9b3b5e29283baa7914d811ed30971273ec8e1
lamtung2712 <ntungnam2789@gmail.comm>
2023-09-18 00:19:40 +0700

    add infor

--- StaffProfile/views.py
+++ StaffProfile/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Staff
 
-
-
 def staff_detail(request, staff_id):
     staff = get_object_or_404(Staff, id=staff_id)
-    return render(request, 'homepage/staff_list.html', {'staff': staff})
+    return render(request, 'homepage/contact.html', {'staff': staff})
 
 
 
