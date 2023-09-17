758b145869e9625ed710eba78d90740cd2b646d1
npvinh04 <nqvinh1995@gmail.com>
2023-09-17 22:18:06 +0700

    fix show images for media

--- Store/urls.py
+++ Store/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,3 +27,7 @@ urlpatterns = [
     path('cart/', include('Order.urls', namespace='carts')),
     path('checkout/', include('Payment.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
