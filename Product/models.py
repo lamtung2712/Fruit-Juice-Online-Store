087e87dac88f91250428868b096363508af0689d
lamtung2712 <ntungnam2789@gmail.comm>
2023-09-24 10:42:15 +0700

    modified

--- Product/models.py
+++ Product/models.py
@@ -21,12 +21,13 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
     currency = models.CharField(max_length=200, default='vnd')
-    images = models.ImageField(blank=True,upload_to='img/')
+    images = models.ImageField(blank=True,upload_to='Product/media/img/')
 
 class Discount(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, default='')
     discount = models.IntegerField(default=0)
     active = models.BooleanField(default='True')
+
 
 
--- StaffProfile/views.py
+++ StaffProfile/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Staff
+from Product.models import Product
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+
 
 def staff_detail(request, staff_id):
     staff = get_object_or_404(Staff, id=staff_id)
     return render(request, 'homepage/contact.html', {'staff': staff})
+
+
+def pageView(request):
+    context = {}
+    product_list = Product.objects.filter(status=1).ordered_by('-created')
+
+    # So san pham moi trang
+    per_page = 10
+    paginator = Paginator(product_list, per_page)
+
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except EmptyPage:
+        # Neu page khong phai la so nguyen, hien thi trang dau tien
+        posts = paginator.page(1)
+    except PageNotAnInteger:
+        posts = paginator.page(paginator.num_pages)
+
+    context.update({
+        'product_list': product_list
+    })
+    return render(request, 'homepage/shop.html', context)
 
 
 
